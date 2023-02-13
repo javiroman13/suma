541ce963eb7b233c42e942bcfc0bc16e29a7b978
javier roman <22670018@itiguala.edu.mx>
2023-02-13 12:32:39 -0600

    se crea clase Guitar

---
+++ src/Guitar.java
@@ -0,0 +1,10 @@
+public class Guitar {
+    private String SerialNumber;
+    private double price;
+    private String builder;
+    private String model;
+    private String type;
+    private String backwood;
+    private String topwood;
+
+}//fin
