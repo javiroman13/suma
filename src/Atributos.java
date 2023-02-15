0b1e7068525c4c332edf976f16f92af107f27831
javier roman <22670018@itiguala.edu.mx>
2023-02-14 20:27:26 -0600

    se crean clase atributos

---
+++ src/Atributos.java
@@ -0,0 +1,24 @@
+public class Atributos {
+    //joya
+    private String type; //tipo de joya
+    private String model; //modelo
+    private String material; //material
+    private float price; // precio
+    private double invoice; //folio
+
+    //consultorio
+    private String supplies;//insumos
+    private float ConsultationFee; //costo consulta
+    private int NumberOfPatients; //numero de pacientes
+    private String TypeOfOffice; //tipo de consultorio
+
+    //fruta
+
+    private String tipo;
+    private String CodigoDeFruta;
+    private float precio;
+    private int NumeroDeInvetario;
+
+
+
+}
---
+++ src/inventory.java
@@ -0,0 +1,6 @@
+
+import java.util.List;
+
+public class inventory {
+   private List<Guitar> Guitars;
+}
