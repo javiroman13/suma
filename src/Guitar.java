2f50002a304f637661d854d987e5bf9009dd0c7d
javier roman <22670018@itiguala.edu.mx>
2023-03-16 17:56:28 -0600

    actualizado

--- src/Guitar.java
+++ src/Guitar.java
@@ -7,4 +7,63 @@ public class Guitar {
     private String backwood;
     private String topwood;
 
+    public Guitar() {
+    }
+
+
+    public String getSerialNumber() {
+        return SerialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        SerialNumber = serialNumber;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBuilder() {
+        return builder;
+    }
+
+    public void setBuilder(String builder) {
+        this.builder = builder;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBackwood() {
+        return backwood;
+    }
+
+    public void setBackwood(String backwood) {
+        this.backwood = backwood;
+    }
+
+    public String getTopwood() {
+        return topwood;
+    }
+
+    public void setTopwood(String topwood) {
+        this.topwood = topwood;
+    }
 }//fin
---
+++ src/Student.java
@@ -0,0 +1,64 @@
+public class Student {
+    private Integer ControlNumber;
+    private String Name;
+    private String LastName;
+    private String career;
+    private String Semester;
+
+    public void setControlNumber(Integer controlNumber) {
+        ControlNumber = controlNumber;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getCareer() {
+        return this.career;
+    }
+
+    public void setCareer(String career) {
+        this.care
+    er = career;
+    }
+
+    public String getSemester() {
+        return Semester;
+    }
+
+    public void setSemester(String semester) {
+        Semester = semester;
+    }
+
+    public double getGeneralAverage(Integer controlNumber) {
+        return 0.0 ;
+
+    }
+        public long getControlNumber() {
+        return ControlNumber;
+    }
+
+    public double getSemesterAverage(Integer controlNumber, Integer Semester) {
+        return 0.0;
+    }
+
+    public double getSubjectGrade(Integer controlNumber) {
+        return 0.0;
+    }
+
+
+
+
+}
